e53228d728da36cf37403fc0d8d21d59abae5b75
raorafarhan <raorafarhan@gmail.com>
2022-09-21 16:55:52 +0700

    commit

--- features/class/entity.go
+++ features/class/entity.go
@@ -3,5 +3,18 @@ package class
 type ClassCore struct {
 	ID         uint
 	Nama_Class string
-	Id_User    uint
+}
+
+type UsecaseInterface interface {
+	GetAllClass() (data []ClassCore, err error)
+	PostLogin(data ClassCore) (token string, err error)
+	PostData(data ClassCore) (row int, err error)
+	PutData(data ClassCore) (row int, err error)
+}
+
+type DataInterface interface {
+	SelectAllClassr() (data []ClassCore, err error)
+	InsertData(data ClassCore) (row int, err error)
+	LoginClass(data ClassCore) (token string, err error)
+	UpdateClass(data ClassCore) (row int, err error)
 }
